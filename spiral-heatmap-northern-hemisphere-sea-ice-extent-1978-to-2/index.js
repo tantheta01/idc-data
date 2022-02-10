76d68497c47991b72c84ca6e2b01e60708dbf43a
tantheta01 <greettanay@gmail.com>
2022-02-10 17:51:55 +0530

    Added url

--- spiral-heatmap-northern-hemisphere-sea-ice-extent-1978-to-2/index.js
+++ spiral-heatmap-northern-hemisphere-sea-ice-extent-1978-to-2/index.js
@@ -1,1 +1,1 @@
-export {default} from "./15d6b119ed0c979d@205.js";
+export {default} from "https://raw.githubusercontent.com/tantheta01/idc-data/master/spiral-heatmap-northern-hemisphere-sea-ice-extent-1978-to-2/15d6b119ed0c979d@205.js";
